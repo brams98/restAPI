874978bfe427d5aa6675deb2bfa9339c536c61ea
bram <rahman.sumirat@binus.ac.id>
2018-01-03 00:27:53 +0700

    push first

---
+++ book.php
@@ -0,0 +1,31 @@
+<?php 
+	
+	 header("Access-Control-Allow-Origin: *");
+	 $server = "sql12.freemysqlhosting.net";
+$username = "sql12213686";
+$pass = "Y6WRfr392k";
+$db = "sql12213686";
+$connection = mysqli_connect($server, $username, $pass, $db);
+if ($_REQUEST) {
+$search_query = $_REQUEST['q'];
+$query_result = $connection->query("SELECT * FROM book WHERE title LIKE '%$search_query%' ");
+}else{
+	$query_result = $connection->query("SELECT * FROM book");
+}
+$book = [];
+$count = 0;
+$books = new stdClass();
+
+while ($data = $query_result->fetch_assoc()) {
+	$book[] = $data;
+	$count=$count+1;
+}
+$books->count = $count;
+if ($_REQUEST) {
+$books->q = $search_query;
+}
+$books->book= $book;
+header('Content-Type: application/json');
+print json_encode($books);
+
+	?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+		
+echo "welcome to our web servicee";
+
+echo "aurel bram biankaa stefani";
+
+
+?>
+
